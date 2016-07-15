936c8e15cf0ee6f756274b80a91cd204a68b0767
Paul Lazo <paullazo19@gmail.com>
2016-07-15 09:40:54 -0500

    more icon revisions

--- modules/DetailView.js
+++ modules/DetailView.js
@@ -7,20 +7,20 @@ export default React.createClass({
       <main>
         <div className="detailView">
           <div className="detailView__header">
-            <Link to="/"><i className="fa fa-arrow-left fa-3x detailView__backIcon"></i></Link><img className="detailView__photo" src={`/images/${this.props.params.photo}`}/>
+            <Link to="/"><i className="fa fa-arrow-left fa-5x detailView__backIcon"></i></Link><img className="detailView__photo" src={`/images/${this.props.params.photo}`}/>
           </div>
           <ul className="detailView__list">
             <li className="detailView__contactItem">
-              <i className="fa fa-user fa-3x detailView__contactIcon"></i><p className="detailView__contactInfo detailView__contactName">&nbsp;{this.props.params.firstName} {this.props.params.lastName}</p>
+              <i className="fa fa-user fa-5x detailView__contactIcon"></i><p className="detailView__contactInfo detailView__contactName">&nbsp;{this.props.params.firstName} {this.props.params.lastName}</p>
             </li>
             <li className="detailView__contactItem">
-              <i className="fa fa-envelope fa-3x detailView__contactIcon"></i><p className="detailView__contactInfo detailView__contactEmail">&nbsp;{this.props.params.email} </p>
+              <i className="fa fa-envelope fa-5x detailView__contactIcon"></i><p className="detailView__contactInfo detailView__contactEmail">&nbsp;{this.props.params.email} </p>
             </li>
             <li className="detailView__contactItem">
               <i className="fa fa-mobile-phone fa-5x detailView__contactIcon"></i><p className="detailView__contactInfo">&nbsp;{this.props.params.phoneNumber} </p>
             </li>
             <li className="detailView__contactItem">
-              <i className="fa fa-globe fa-3x detailView__contactIcon"></i><p className="detailView__contactInfo ">&nbsp;{this.props.params.city}, <span className="detailView__contactState">{this.props.params.state}</span></p>
+              <i className="fa fa-globe fa-5x detailView__contactIcon"></i><p className="detailView__contactInfo ">&nbsp;{this.props.params.city}, <span className="detailView__contactState">{this.props.params.state}</span></p>
             </li>
           </ul>
         </div>
